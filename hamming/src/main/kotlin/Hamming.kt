481594680dbcea926c93784e7c85af696e76b276
rapidfireworks <rapidfireworks@gmail.com>
2021-12-13 17:24:41 +0900

    Solve with count

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,10 @@
 object Hamming {
 
-    fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+  fun compute(leftStrand: String, rightStrand: String): Int {
+    if (leftStrand.length == rightStrand.length) {
+      return leftStrand.zip(rightStrand).count { (lhs, rhs) -> lhs != rhs }
+    } else {
+      throw IllegalArgumentException("left and right strands must be of equal length")
     }
+  }
 }
