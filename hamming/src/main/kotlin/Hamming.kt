afd4136da2056b583dccd6f740bd00d705b880c8
rapidfireworks <rapidfireworks@gmail.com>
2021-12-14 10:27:41 +0900

    Solve with BreakIterator

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,16 +1,54 @@
+import java.text.BreakIterator
+
 object Hamming {
 
   fun compute(leftStrand: String, rightStrand: String): Int {
-    if (leftStrand.length == rightStrand.length) {
-      var result = 0
-      for ((lhs, rhs) in leftStrand zip rightStrand) {
-        if (lhs != rhs) {
-          result += 1
-        }
+    var result = 0
+    val rightGraphemes = Graphemes(rightStrand)
+    for (lhs in Graphemes(leftStrand)) {
+      if (!rightGraphemes.hasNext()) {
+        throw DifferentLengthException()
+      } else if (lhs != rightGraphemes.next()) {
+        result += 1
       }
-      return result
-    } else {
-      throw IllegalArgumentException("left and right strands must be of equal length")
     }
+    if (rightGraphemes.hasNext()) {
+      throw DifferentLengthException()
+    } else {
+      return result
+    }
+  }
+
+  class DifferentLengthException :
+      IllegalArgumentException("left and right strands must be of equal length") {}
+}
+
+class Graphemes : Iterable<String>, Iterator<String> {
+
+  val text: String
+  val iterator: BreakIterator
+  var startIndex: Int
+  var endIndex: Int
+
+  constructor(text: String) {
+    this.text = text
+    this.iterator = BreakIterator.getCharacterInstance().apply { setText(text) }
+    this.startIndex = this.iterator.first()
+    this.endIndex = this.iterator.next()
+  }
+
+  override operator fun iterator(): Iterator<String> {
+    return this
+  }
+
+  override operator fun hasNext(): Boolean {
+    return endIndex != BreakIterator.DONE
+  }
+
+  override operator fun next(): String {
+    val result = text.substring(startIndex, endIndex)
+    startIndex = endIndex
+    endIndex = iterator.next()
+    return result
   }
 }
