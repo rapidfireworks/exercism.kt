076ad9b7f77e0eb499f46b27cff28d90e4e200fb
rapidfireworks <rapidfireworks@gmail.com>
2021-12-20 16:07:35 +0900

    Solve

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,35 @@
-class Matrix(private val matrixAsString: String) {
+data class Matrix(val matrix: List<List<Int>>) {
 
-    fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
-    }
+  constructor(
+      matrix: String
+  ) : this(parse(matrix, Regex("""\p{Z}*\n\p{Z}*"""), Regex("""\p{Z}+""")))
 
-    fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+  companion object {
+    fun parse(matrix: String, lnRegex: Regex, spRegex: Regex): List<List<Int>> {
+      var result = mutableListOf<List<Int>>()
+      for (line in matrix.split(lnRegex)) {
+        var elements = mutableListOf<Int>()
+        for (letter in line.split(spRegex)) {
+          val element = letter.toIntOrNull()
+          if (element != null) {
+            elements.add(element)
+          }
+        }
+        result.add(elements)
+      }
+      return result
     }
+  }
+
+  fun column(colNr: Int): List<Int> {
+    var result = mutableListOf<Int>()
+    for (line in matrix) {
+      result.add(line[colNr - 1])
+    }
+    return result
+  }
+
+  fun row(rowNr: Int): List<Int> {
+    return matrix[rowNr - 1]
+  }
 }
