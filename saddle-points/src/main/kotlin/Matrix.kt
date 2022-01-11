6bb4de3f636cca01721b5dc67b19a1d7c0d35010
rapidfireworks <rapidfireworks@gmail.com>
2022-01-11 10:00:18 +0900

    Refactor

--- saddle-points/src/main/kotlin/Matrix.kt
+++ saddle-points/src/main/kotlin/Matrix.kt
@@ -8,37 +8,37 @@ data class Matrix(val matrix: List<List<Int>>) {
 
   companion object {
     fun saddlePoints(matrix: List<List<Int>>): Set<MatrixCoordinate> {
-      if (matrix.isEmpty()) {
-        return setOf()
-      } else {
-        val maxOfRows = matrix.mapNotNull { it.maxOrNull() }
-        val minOfCols = minOfCols(matrix)
-        val result = mutableSetOf<MatrixCoordinate>()
-        for (row in matrix.indices) {
-          for (col in matrix[row].indices) {
-            val value = matrix[row][col]
-            if (value == maxOfRows[row] && value == minOfCols[col]) {
-              result.add(MatrixCoordinate(row + 1, col + 1))
-            }
+      val maxOfRows = matrix.mapNotNull { it.maxOrNull() }
+      val minOfCols = minOfCols(matrix)
+      val result = mutableSetOf<MatrixCoordinate>()
+      for (row in matrix.indices) {
+        for (col in matrix[row].indices) {
+          val value = matrix[row][col]
+          if (maxOfRows[row] <= value && value <= minOfCols[col]) {
+            result.add(MatrixCoordinate(row + 1, col + 1))
           }
-        }
-        return result
-      }
-    }
-
-    fun minOfCols(matrix: List<List<Int>>): List<Int> {
-      val result = mutableListOf<Int>()
-      for (col in matrix[0].indices) {
-        val values = mutableListOf<Int>()
-        for (row in matrix.indices) {
-          values.add(matrix[row][col])
-        }
-        val value = values.minOrNull()
-        if (value != null) {
-          result.add(value)
         }
       }
       return result
+    }
+
+    fun minOfCols(matrix: List<List<Int>>): List<Int> {
+      if (matrix.isEmpty()) {
+        return listOf()
+      } else {
+        val result = mutableListOf<Int>()
+        for ((index, value) in matrix[0].withIndex()) {
+          var minValue = value
+          for (row in matrix.drop(1)) {
+            val newValue = row[index]
+            if (newValue < minValue) {
+              minValue = newValue
+            }
+          }
+          result.add(minValue)
+        }
+        return result
+      }
     }
   }
 }
