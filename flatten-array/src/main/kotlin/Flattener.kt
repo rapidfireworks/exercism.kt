cc117c3ebae8f0443a9834bccbe2fadaf0cf781b
rapidfireworks <rapidfireworks@gmail.com>
2022-01-13 09:11:52 +0900

    Solve

--- flatten-array/src/main/kotlin/Flattener.kt
+++ flatten-array/src/main/kotlin/Flattener.kt
@@ -1,5 +1,16 @@
 object Flattener {
-    fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+
+  fun flatten(source: Collection<Any?>): List<Any> {
+    return mutableListOf<Any>().also { flatten(source, it) }
+  }
+
+  fun flatten(source: Collection<Any?>, result: MutableList<Any>) {
+    for (element in source) {
+      when (element) {
+        null -> continue
+        is Collection<Any?> -> flatten(element, result)
+        else -> result.add(element)
+      }
     }
+  }
 }
