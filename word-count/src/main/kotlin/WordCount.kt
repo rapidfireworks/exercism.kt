db0f8120522f3c1a1ee4fe52fc9b81f9246e377f
rapidfireworks <rapidfireworks@gmail.com>
2022-01-15 11:51:18 +0900

    Solve

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,14 @@
 object WordCount {
 
-    fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+  fun phrase(phrase: String): Map<String, Int> {
+    return phrase(phrase, Regex("""\b[\p{L}\p{N}']+\b"""))
+  }
+
+  fun phrase(phrase: String, regex: Regex): Map<String, Int> {
+    return mutableMapOf<String, Int>().apply {
+      for (result in regex.findAll(phrase)) {
+        result.value.lowercase().let { put(it, getOrDefault(it, 0) + 1) }
+      }
     }
+  }
 }
