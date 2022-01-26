dd49b64f30e6ffc3a55faf4d34d5905f2efbb92e
rapidfireworks <rapidfireworks@gmail.com>
2022-01-26 10:37:32 +0900

    Solve

--- robot-name/src/main/kotlin/RobotName.kt
+++ robot-name/src/main/kotlin/RobotName.kt
@@ -1,9 +1,33 @@
-class Robot {
+data class Robot(var name: String) {
 
-    val name: String
-        get() = TODO("Implement this getter to complete the task")
+  constructor() : this(uniqueName()!!)
 
-    fun reset() {
-        TODO("Implement this function to complete the task")
+  fun reset() {
+    name = uniqueName()!!
+  }
+
+  companion object {
+    val created = mutableSetOf<String>()
+
+    fun uniqueName(): String? {
+      val limit = 26 * 26 * 1000
+      while (created.size <= limit) {
+        val result = name()
+        if (!created.contains(result)) {
+          created.add(result)
+          return result
+        }
+      }
+      return null
     }
+
+    fun name(): String {
+      val builder = StringBuilder()
+      for (_count in 0 until 2) {
+        builder.append(('A'..'Z').random())
+      }
+      builder.append("%03d".format((0 until 1000).random()))
+      return builder.toString()
+    }
+  }
 }
