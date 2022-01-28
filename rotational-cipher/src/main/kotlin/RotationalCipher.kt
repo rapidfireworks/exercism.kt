b45bbbe3c8764e4386c28729c54d5278f9af9619
rapidfireworks <rapidfireworks@gmail.com>
2022-01-28 11:14:27 +0900

    Solve

--- rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,8 +1,19 @@
-class RotationalCipher {
+data class RotationalCipher(val key: Int) {
 
-    // TODO: Implement proper constructor
+  val letters = (('A'..'Z') + ('a'..'z')).toSet()
 
-    fun encode(text: String): String {
-        TODO("Implement this function to complete the task")
-    }
+  fun encode(text: String): String = text.map(::encode).joinToString("")
+
+  fun encode(letter: Char): Char = when (val base = base(letter)) {
+    null -> letter
+    else -> ((letter.code - base + key) % 26 + base).toChar()
+  }
+
+  fun base(letter: Char): Int? = if (!letters.contains(letter)) {
+    null
+  } else if (letter.isUpperCase()) {
+    'A'.code
+  } else {
+    'a'.code
+  }
 }
