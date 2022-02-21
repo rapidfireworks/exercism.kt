96f4f22431a39895fac26ecf9e759081e6a96689
rapidfsub <rapidfsub@gmail.com>
2022-02-21 13:15:05 +0900

    Solve

--- raindrops/src/main/kotlin/Raindrops.kt
+++ raindrops/src/main/kotlin/Raindrops.kt
@@ -1,6 +1,24 @@
 object Raindrops {
 
-    fun convert(n: Int): String {
-        TODO("Implement this function to complete the task")
+  fun convert(n: Int): String {
+    val result = buildString {
+      for (factor in listOf(3, 5, 7)) {
+        sound(n, factor)?.let(::append)
+      }
     }
+    return result.ifEmpty {
+      n.toString()
+    }
+  }
+
+  fun sound(n: Int, factor: Int): String? = if (n % factor == 0) {
+    when (factor) {
+      3 -> "Pling"
+      5 -> "Plang"
+      7 -> "Plong"
+      else -> null
+    }
+  } else {
+    null
+  }
 }
