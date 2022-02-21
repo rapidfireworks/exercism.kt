934e6912057a4aace1df14e1ac20a3efcf865748
rapidfsub <rapidfsub@gmail.com>
2022-02-22 07:24:28 +0900

    Solve

--- isogram/src/main/kotlin/Isogram.kt
+++ isogram/src/main/kotlin/Isogram.kt
@@ -1,6 +1,16 @@
 object Isogram {
 
-    fun isIsogram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+  fun isIsogram(input: String): Boolean {
+    val letters = mutableSetOf<Char>()
+    for (letter in input.lowercase()) {
+      if (letter.isLowerCase()) {
+        if (letters.contains(letter)) {
+          return false
+        } else {
+          letters.add(letter)
+        }
+      }
     }
+    return true
+  }
 }
